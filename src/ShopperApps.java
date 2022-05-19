a104c80898b1bc522af2ec36807564eeffb64608
avrians <avriansyahb10@gmail.com>
2022-05-19 20:59:01 +0700

    master

--- src/ShopperApps.java
+++
@@ -1,6 +0,0 @@
-// public class ShopperApps {
-//     public static void main(String[] args {
-//         String imgLoc = "C:\Users\Avria\OneDrive\Pictures\Kuliah";
-//     //    WindowsGUI 
-//     })
-// }
